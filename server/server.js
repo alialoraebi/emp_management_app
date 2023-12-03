57c80996901238f3b742b6e73ec5e191fbc2faa9
ali <a.aloreabi2000@gmail.com>
2023-12-03 02:50:11 -0500

    Added docker and instructions for running container

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userModel = require("./model/user.js");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 const app = express(); 
 
