4e369a78b42b9183eda5b53ecdd94971ed351fa8
ali <a.aloreabi2000@gmail.com>
2023-12-03 15:52:01 -0500

    added signup component

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // user registration login
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+        placeholder="Username"
+      />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="Password"
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignUp;
